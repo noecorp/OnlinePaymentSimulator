79c1980299f75f027c5ce9a72069c95db37a166a
vh <admin@localhost>
2018-10-13 01:10:31 +0300

    Updated index.html

--- OnlinePaymentSimulator/src/main/java/onlinepaymentsimulator/Main.java
+++ OnlinePaymentSimulator/src/main/java/onlinepaymentsimulator/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("Main.BANK_NAME", Main.BANK_NAME);
+            map.put("BANK_NAME", Main.BANK_NAME);
 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
