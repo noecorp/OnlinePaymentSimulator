2874a0d71a294d53f0eb655e785441a4b6ef32e4
vh <admin@localhost>
2018-10-13 00:30:49 +0300

    Created index.html, added new route ("/") and updated pom.xml with Spark etc. dependencies

--- OnlinePaymentSimulator/src/main/java/onlinepaymentsimulator/Main.java
+++ OnlinePaymentSimulator/src/main/java/onlinepaymentsimulator/Main.java
@@ -1,8 +1,20 @@
 package onlinepaymentsimulator;
 
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 public class Main {
     
+    public static String BANK_NAME = "Demo Pankki";
+    
     public static void main(String[] args) throws Exception {
-        
+        Spark.get("/", (req, res) -> {
+            HashMap map = new HashMap<>();
+            map.put("Main.BANK_NAME", Main.BANK_NAME);
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
     }
 }
